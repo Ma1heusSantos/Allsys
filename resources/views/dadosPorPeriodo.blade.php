4c4eede5ec6e86c5f50df92a732316f9373b72c0
theprincejug <almeidasandro051@gmail.com>
2024-05-21 15:30:28 -0300

    Card de Relatórios por período

--- resources/views/dadosPorPeriodo.blade.php
+++ resources/views/dadosPorPeriodo.blade.php
@@ -2,8 +2,9 @@
 @section('titulo')
     data period
 @endsection
+@section('conteudo')
 
-{{-- <div class="card mx-auto shadow" style="width: 85%">
+<div class="card mx-auto shadow" style="width: 65%;">
     <h2 class="card-title p-3">
         Relatório por período
     </h2>
@@ -27,41 +28,10 @@
                                     <x-Input-error :messages="$errors->get('email')" class="mt-1" />
                                 </div>
                             </div>
-                            <div class="col-md-6">
-                                <div class="form-group p-2">
-                                    <label for="password">Senha</label>
-                                    <input required name='password'class="form-control" type="password">
-                                    <x-Input-error :messages="$errors->get('password')" class="mt-1" />
-
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group p-2">
-                                    <label for="confirmePassword">Confirme sua senha:</label>
-                                    <input required name="confirmePassword" class="form-control" type="password">
-                                    <x-Input-error :messages="$errors->get('confirmePassword')" class="mt-1" />
-
-                                </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="form-group p-3 pb-4">
-                                    <label for="name">Tipo de Usuário</label>
-                                    <select required name="role" class="form-select"
-                                        aria-label="Default select example">
-                                        <option selected>Selecione o tipo de usuário</option>
-                                        <option value="User">User</option>
-                                        <option value="Admin">Admin</option>
-                                    </select>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12 mt-2">
-                                    <button class="btn btn-primary w-100">Salvar</button>
-                                </div>
-                            </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
-    </div> --}}
+    </div>
+@endsection
