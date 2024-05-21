d5a0a4e3550297008285aa1c8a6ab7a202481e75
theprincejug <almeidasandro051@gmail.com>
2024-05-21 10:34:27 -0300

    Visualização do layout 100%

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
@@ -17,14 +17,14 @@
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://code.highcharts.com/modules/cylinder.js"></script>
 
-    
-   
+
+
 
     <title> @yield('title')</title>
 
 </head>
 
-<body>
+<body style="width: 100%; height: 100%;">
     <div>
         <x-Sidebar>
             @yield('conteudo')
