227d0a2c45e45516ce94a4cd7e699a72944e7acc
Matheus <matheuscontato96@hotmail.com>
2024-05-23 14:57:51 -0300

    FEAT: criando paginate

--- resources/views/vendasPorPeriodo.blade.php
+++ resources/views/vendasPorPeriodo.blade.php
@@ -2,67 +2,78 @@
 @section('titulo')
     data period
 @endsection
+
 @section('conteudo')
-    <div class="card mx-auto shadow w-75 w-md-50">
+
+    <div class="card mx-auto shadow w-100 p-0">
         <h2 class="card-title p-3 text-primary">
             Relatório por período
         </h2>
         <div class="card-body">
-            <div class="row">
-                <div class="row col-12">
-                    <div class="row form-group">
-                        <form action="{{ route('getVendasDia') }}" method="post">
-                            @csrf
-                            <div class="row d-flex flex-row">
-                                <div class="col-md-12">
-                                    <div class="form-group p-2">
-                                        <label for="dataini">Data de Início</label>
-                                        <input required name="dataini" class="form-control" type="date">
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group p-2">
-                                        <label for="datafim">Data de Fim</label>
-                                        <input required name="datafim" class="form-control" type="date">
-                                    </div>
-                                </div>
-                                <div class="col-md-12">
-                                    <div class="form-group p-2 mt-2">
-                                        <button class="btn btn-primary w-50">Enviar</button>
-                                    </div>
-                                </div>
+            <div class="form-group">
+                <form action="{{ route('getVendasDia') }}" method="post">
+                    @csrf
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group p-2">
+                                <label for="dataini">Data de Início</label>
+                                <input required name="dataini" class="form-control" type="date">
                             </div>
-                        </form>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group p-2">
+                                <label for="datafim">Data de Fim</label>
+                                <input required name="datafim" class="form-control" type="date">
+                            </div>
+                        </div>
                     </div>
-                    <hr>
-                    <div>
-                        @isset($dados)
-                            <table class="table table-hover mt-5">
-                                <thead>
-                                    <tr>
-                                        <th>Cód.de venda</th>
-                                        <th>Produto</th>
-                                        <th>Preço de Venda</th>
-                                        <th>Total da venda</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($dados as $dado)
-                                        <tr>
-                                            <td>{{ $dado->codvenda }}</td>
-                                            <td>{{ $dado->dscprod }}</td>
-                                            <td>{{ $dado->precoitemvenda }}</td>
-                                            <td>{{ $dado->totalitemvenda }}</td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        @else
-                            <p>Nenhum produto foi vendido nesse dia</p>
-                        @endisset
+                    <div class="row">
                     </div>
-                </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group p-2 mt-2">
+                                <button class="btn btn-primary w-100">Enviar</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <hr>
+            <div class="table-responsive">
+                @isset($dados)
+                    <table id="table" class="">
+                        <thead>
+                            <tr>
+                                <th>Cód.de venda</th>
+                                <th>Produto</th>
+                                <th>Preço de Venda</th>
+                                <th>Total da venda</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($dados as $dado)
+                                <tr>
+                                    <td>{{ $dado->codvenda }}</td>
+                                    <td>{{ $dado->dscprod }}</td>
+                                    <td>{{ $dado->precoitemvenda }}</td>
+                                    <td>{{ $dado->totalitemvenda }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @else
+                    <p>Nenhum produto foi vendido nesse dia</p>
+                @endisset
             </div>
         </div>
     </div>
+    </div>
+    </div>
+@endsection
+@section('script')
+    <script>
+        $(document).ready(function() {
+            $('#table').DataTable();
+        });
+    </script>
 @endsection
