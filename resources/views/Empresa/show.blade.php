fa294500546393d76a099ac2cc98e2f732e0d220
Matheus <matheuscontato96@hotmail.com>
2024-06-05 17:47:11 -0300

    FIX: corrigindo classe bootstrap

--- resources/views/Empresa/show.blade.php
+++ resources/views/Empresa/show.blade.php
@@ -17,38 +17,38 @@
             <ul class="list-group llist-group-horizontal-sn">
                 <li class="list-group-item d-flex flex-row justify-content-around">
                     <div class="m-0">
-                    <p class="text-dark font-weight-bold d-inline mr-2" style="font-size:14px">Razao Social: </p>
+                    <p class="text-dark fw-bold d-inline mr-2" style="font-size:14px">Razao Social: </p>
                     <p class="d-inline text-dark font-weight-normal" style="font-size:11px" > {{ $empresa->nomeposto ?? 'não informado' }}</p>
                     </div>
                     <div class="m-auto">
-                        <p class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">Cnpj: </p>
+                        <p class="text-dark fw-bold d-inline mr-2" style="font-size: 14px">Cnpj: </p>
                         <p class="d-inline text-dark font-weight-normal"style="font-size:11px">{{ $empresa->contabilidadeCnpj ?? 'não informado' }}</p>
                     </div>                     
                 </li>
                 <li class="list-group-item">
-                    <p class="text-dark font-weight-bold d-inline mr-2" style="font-size:14px">Endereço: </p>
+                    <p class="text-dark fw-bold   d-inline mr-2" style="font-size:14px">Endereço: </p>
                     <p class="d-inline text-dark font-weight-normal" style="font-size:11px" >{{ $empresa->endposto ?? 'não informado' }}</p>
                 </li>
                 <li class="list-group-item d-flex flex-row justify-content-around">
                     <div class="m-0">
-                        <p class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">Cep: </p>
+                        <p class="text-dark fw-bold   d-inline mr-2" style="font-size: 14px">Cep: </p>
                         <p class="d-inline text-dark font-weight-normal" style="font-size:11px">{{ $empresa->cepposto ?? 'não informado' }}</p>
                     </div>
                     <div class="m-auto">
-                        <p class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">E-mail: </p>
+                        <p class="text-dark fw-bold   d-inline mr-2" style="font-size: 14px">E-mail: </p>
                         <p class="d-inline text-dark font-weight-normal"style="font-size:11px">{{ $empresa->emailposto ?? 'não informado' }}</p>
                     </div>
                 </li>
                 <li class="list-group-item">
-                    <p class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">Inscrição Estadual </p>
+                    <p class="text-dark fw-bold   d-inline mr-2" style="font-size: 14px">Inscrição Estadual </p>
                     <p class="d-inline text-dark font-weight-normal"style="font-size:11px">{{ $empresa->ieposto ?? 'não informado' }}</p>
                 </li>
                 <li class="list-group-item">
-                    <p class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">Responsavel: </p>
+                    <p class="text-dark fw-bold   d-inline mr-2" style="font-size: 14px">Responsavel: </p>
                     <p class="d-inline text-dark font-weight-normal"style="font-size:11px">{{ $empresa->responsavel ?? 'não informado' }}</p>
                 </li>
                 <li class="list-group-item">
-                    <h2 class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">Telefone: </h2>
+                    <h2 class="text-dark fw-bold   d-inline mr-2" style="font-size: 14px">Telefone: </h2>
                     <p class="d-inline text-dark font-weight-normal"style="font-size:11px">{{ $empresa->telposto ?? 'não informado' }}</p>
                 </li>
             </ul>
