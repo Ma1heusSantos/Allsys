239c2a042ae661e5fd7d7c009f8449ef4d0ba447
thales1977 <geniusisal@gmail.com>
2024-06-05 17:31:11 -0300

    ajuste layout tela company

--- resources/views/Empresa/show.blade.php
+++ resources/views/Empresa/show.blade.php
@@ -3,54 +3,53 @@
     Empresa
 @endsection
 @section('conteudo')
-    <div class="card">
+    <div class="card shadow">
         <div class="card-header">
             <section>
                 <div class="row px-4 pt-4">
                     <h1 class="text-lg text-indigo" style="font-weight: 500">
                         <span class="fw-bold text-primary">{{ $empresa->fantasiaposto ?? 'não informado' }}</span>
                     </h1>
-                </div>
-                <div class="row d-flex justify-content-between">
-                    <div class="col-md-8 d-flex justify-content-between">
-                        <p class="text-indigo font-weight-normal px-4 fs-6">Nome da empresa:
-                            {{ $empresa->nomeposto ?? 'não informado' }}
-                        </p>
-                        <p class="text-indigo font-weight-normal ml-2">
-                            CNPJ: <span
-                                class="text-dark font-weight-normal">{{ $empresa->contabilidadeCnpj ?? 'não informado' }}</span>
-                        </p>
-                    </div>
-                </div>
+                </div>      
             </section>
         </div>
         <div class="card-body">
-            <ul class="list-group list-group-flush">
+            <ul class="list-group llist-group-horizontal-sn">
+                <li class="list-group-item d-flex flex-row justify-content-around">
+                    <div class="m-0">
+                    <p class="text-dark font-weight-bold d-inline mr-2" style="font-size:14px">Razao Social: </p>
+                    <p class="d-inline text-dark font-weight-normal" style="font-size:11px" > {{ $empresa->nomeposto ?? 'não informado' }}</p>
+                    </div>
+                    <div class="m-auto">
+                        <p class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">Cnpj: </p>
+                        <p class="d-inline text-dark font-weight-normal"style="font-size:11px">{{ $empresa->contabilidadeCnpj ?? 'não informado' }}</p>
+                    </div>                     
+                </li>
                 <li class="list-group-item">
-                    <h2 class="text-dark font-weight-bold d-inline mr-2" style="font-size: 20px">Endereço: </h2>
-                    <p class="d-inline text-dark font-weight-normal">{{ $empresa->endposto ?? 'não informado' }}</p>
+                    <p class="text-dark font-weight-bold d-inline mr-2" style="font-size:14px">Endereço: </p>
+                    <p class="d-inline text-dark font-weight-normal" style="font-size:11px" >{{ $empresa->endposto ?? 'não informado' }}</p>
                 </li>
                 <li class="list-group-item d-flex flex-row justify-content-around">
                     <div class="m-0">
-                        <h2 class="text-dark font-weight-bold d-inline mr-2" style="font-size: 20px">CEP: </h2>
-                        <p class="d-inline text-dark font-weight-normal">{{ $empresa->cepposto ?? 'não informado' }}</p>
+                        <p class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">Cep: </p>
+                        <p class="d-inline text-dark font-weight-normal" style="font-size:11px">{{ $empresa->cepposto ?? 'não informado' }}</p>
                     </div>
                     <div class="m-auto">
-                        <h2 class="text-dark font-weight-bold d-inline mr-2" style="font-size: 20px">E-mail: </h2>
-                        <p class="d-inline text-dark font-weight-normal">{{ $empresa->emailposto ?? 'não informado' }}</p>
+                        <p class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">E-mail: </p>
+                        <p class="d-inline text-dark font-weight-normal"style="font-size:11px">{{ $empresa->emailposto ?? 'não informado' }}</p>
                     </div>
                 </li>
                 <li class="list-group-item">
-                    <h2 class="text-dark font-weight-bold d-inline mr-2" style="font-size: 20px">Inscrição Estadual </h2>
-                    <p class="d-inline text-dark font-weight-normal">{{ $empresa->ieposto ?? 'não informado' }}</p>
+                    <p class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">Inscrição Estadual </p>
+                    <p class="d-inline text-dark font-weight-normal"style="font-size:11px">{{ $empresa->ieposto ?? 'não informado' }}</p>
                 </li>
                 <li class="list-group-item">
-                    <h2 class="text-dark font-weight-bold d-inline mr-2" style="font-size: 20px">Responsavel: </h2>
-                    <p class="d-inline text-dark font-weight-normal">{{ $empresa->responsavel ?? 'não informado' }}</p>
+                    <p class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">Responsavel: </p>
+                    <p class="d-inline text-dark font-weight-normal"style="font-size:11px">{{ $empresa->responsavel ?? 'não informado' }}</p>
                 </li>
                 <li class="list-group-item">
-                    <h2 class="text-dark font-weight-bold d-inline mr-2" style="font-size: 20px">Telefone: </h2>
-                    <p class="d-inline text-dark font-weight-normal">{{ $empresa->telposto ?? 'não informado' }}</p>
+                    <h2 class="text-dark font-weight-bold d-inline mr-2" style="font-size: 14px">Telefone: </h2>
+                    <p class="d-inline text-dark font-weight-normal"style="font-size:11px">{{ $empresa->telposto ?? 'não informado' }}</p>
                 </li>
             </ul>
         </div>
