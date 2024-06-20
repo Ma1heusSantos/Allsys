f8898f1284c5b19650df001404f11255aaad1e9a
Matheus <matheuscontato96@hotmail.com>
2024-06-20 16:15:42 -0300

    FIX: validando campos do troca preço

--- resources/views/components/Rodape.blade.php
+++ resources/views/components/Rodape.blade.php
@@ -1,32 +1,30 @@
-  
   <style>
-    /* Estilos personalizados para o footer */
-    footer {
-      background-color: #b9c8f4 !important;
-      background-image: linear-gradient(180deg, #507ef3 10%, #224abe 100%) !important;
-      background-size: cover;
-      color: #fff;
-      padding: 24px 0;
-      position: fixed;
-      bottom: 3px;
-      width: 100%;
-      height: 10px;
-      border-radius: 20px;
-    }
-    #textoRodape{
-      margin-bottom: 6px !important;
-        
+      /* Estilos personalizados para o footer */
+      footer {
+          background-color: #b9c8f4 !important;
+          background-image: linear-gradient(180deg, #507ef3 10%, #224abe 100%) !important;
+          background-size: cover;
+          color: #fff;
+          padding: 24px 0;
+          position: fixed;
+          bottom: 3px;
+          width: 100%;
+          height: 10px;
+          border-radius: 20px;
+      }
 
-    }
+      #textoRodape {
+          margin-bottom: 6px !important;
+
+
+          #textoRodape {
+              margin-bottom: 6px !important;
+          }
   </style>
-</head>
+  </head>
   <!-- Footer -->
   <footer class="d-flex text-center align-items-center justify-content-center">
-    <img class="fluid" src="{{ asset('img/logoVL.svg') }}" alt="Bootstrap" width="80" height="50">
-     <p class="text-decoration-none  text-white" id="textoRodape"> &copy; 2024 Vltech. Todos os direitos reservados.</p>
-     <img class="fluid" src="{{ asset('img/logoG.svg') }}" alt="Bootstrap" width="80" height="50">
+      <img class="fluid" src="{{ asset('img/logoVL.svg') }}" alt="Bootstrap" width="80" height="50">
+      <p class="text-decoration-none  text-white" id="textoRodape"> &copy; 2024 Vltech. Todos os direitos reservados.</p>
+      <img class="fluid" src="{{ asset('img/logoG.svg') }}" alt="Bootstrap" width="80" height="50">
   </footer>
-
- 
-
-
--- resources/views/produtos/trocaPreco.blade.php
+++ resources/views/produtos/trocaPreco.blade.php
@@ -15,7 +15,7 @@
                 <div class="d-flex flex-column flex-sm-row align-items-center">
                     <div class="row g-3 align-items-center mx-1">
                         <div class="col-12 col-sm-auto">
-                            <label for="dataTroca" class="col-form-label">Data de fim:</label>
+                            <label for="dataTroca" class="col-form-label">Data de Troca:</label>
                         </div>
                         <div class="col-12 col-sm-auto d-flex">
                             <input type="date" id="dataTroca" name="dataTroca" class="form-control">
@@ -32,7 +32,8 @@
                         <div class="row" id="inputs-container">
                             @foreach ($dadosResponse as $dado)
                                 <div class="col-md-4 mb-3 d-flex">
-                                    <div class="card shadow text-center h-100" style="width: 20rem; background-color: #b9c8f4;background-image: linear-gradient(180deg, #4787e1 10%, #197beaee 100%);background-size: cover;color: #ddf0e7;" >
+                                    <div class="card shadow text-center h-100"
+                                        style="width: 20rem; background-color: #b9c8f4;background-image: linear-gradient(180deg, #4787e1 10%, #197beaee 100%);background-size: cover;color: #ddf0e7;">
                                         <div class="card-body">
                                             <h5 class="card-title">{{ $dado->dscprod }}</h5>
                                             <input hidden type="text" id="codigo-{{ $loop->index }}"
@@ -42,12 +43,12 @@
                                             <input hidden type="text" id="codemp-{{ $loop->index }}"
                                                 value="{{ $dado->codemp }}">
 
-                                            <label for="">A vista - R$ {{ money($dado->avista) }} </label>
-                                            <input class="form-control" type="text"
+                                            <label for="">A vista - R$ {{ $dado->avista }} </label>
+                                            <input class="form-control" type="number"
                                                 placeholder="Digite o novo preço a vista aqui"
                                                 id="avista-{{ $loop->index }}" name="avista-{{ $loop->index }}">
-                                            <label for=""> A Prazo - R$ {{ money($dado->aprazo) }}</label>
-                                            <input class="form-control" type="text"
+                                            <label for=""> A Prazo - R$ {{ $dado->aprazo }}</label>
+                                            <input class="form-control" type="number"
                                                 placeholder="Digite o novo preço a prazo aqui"
                                                 id="aprazo-{{ $loop->index }}" name="aprazo-{{ $loop->index }}">
                                         </div>
@@ -67,6 +68,20 @@
             let btn = document.getElementById('btn');
             const dadosJson = @json($dadosJson);
             const user = @json($usuario);
+            let requestBody = {};
+            let dateInput = document.getElementById('dataTroca');
+
+            dateInput.addEventListener('input', function() {
+                const inputDate = new Date(dateInput.value);
+                const currentDate = new Date();
+
+                if (inputDate < currentDate.setHours(0, 0, 0, 0)) {
+                    alert('A data não pode ser menor que a data atual.');
+                    dateInput.value = '';
+                } else {
+                    let date = tratarData(dateInput.value);
+                }
+            });
 
             btn.addEventListener('click', function() {
                 const data = [];
@@ -79,21 +94,30 @@
                     const aprazoInput = card.querySelector('input[id^="aprazo"]');
                     const terminalInput = card.querySelector('input[id^="terminal"]');
                     const codempInput = card.querySelector('input[id^="codemp"]');
-                    let date = document.getElementById('dataTroca').value;
+                    let date = tratarData(document.getElementById('dataTroca').value);
 
                     if (codigoInput && avistaInput && aprazoInput && codempInput && terminalInput &&
                         date) {
+
+                        const avista = avistaInput.value.replace(',', '.');
+                        const aprazo = aprazoInput.value.replace(',', '.');
+
+                        if (isNaN(avista) || isNaN(aprazo)) {
+                            alert(
+                                'Por favor, insira valores numéricos válidos para Avista e Aprazo.'
+                                );
+                            return;
+                        }
+
                         const codigo = codigoInput.value;
-                        const avista = avistaInput.value;
-                        const aprazo = aprazoInput.value;
                         const terminal = terminalInput.value;
                         const codemp = codempInput.value;
 
                         const produto = {
-                            codprod: codigo,
-                            avista: avista,
-                            aprazo: aprazo,
-                            datatroca: date.toLocaleString('pt-BR'),
+                            codprod: codigo ? codigo : null,
+                            avista: avista ? avista : null,
+                            aprazo: aprazo ? aprazo : null,
+                            datatroca: date,
                             codemp: codemp,
                             usuario: user.email,
                             terminal: terminal
@@ -101,16 +125,17 @@
 
                         data.push(produto);
                     } else {
-                        alert('informe todos os campos!');
+                        alert('Informe todos os campos!');
+                        return;
                     }
                 });
+                console.log(data);
                 const config = {
                     headers: {
                         Authorization: `Bearer ${user.token}`
                     }
                 };
-                axios.put('http://' + user.cnpj +
-                        '.ddns.net:8098/api/svrpista/bicos/trocapreco', data, config)
+                axios.put(`http://${user.cnpj}.ddns.net:8098/api/svrpista/bicos/trocapreco`, data, config)
                     .then(function(response) {
                         console.log('Dados enviados com sucesso:', response.data);
                         alert('Dados enviados com sucesso!');
@@ -121,5 +146,14 @@
                     });
             });
         });
+
+        function tratarData(date) {
+            const dateObj = new Date(date);
+            const day = String(dateObj.getDate()).padStart(2, '0');
+            const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+            const year = dateObj.getFullYear();
+            const formattedDate = `${day}/${month}/${year}`;
+            return formattedDate;
+        }
     </script>
 @endsection
