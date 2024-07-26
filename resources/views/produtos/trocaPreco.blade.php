f5d5313869bb962e6b6dc22736af394e5346d796
Matheus <matheuscontato96@hotmail.com>
2024-07-26 10:27:14 -0300

    FIX: corrigindo cores do troca preço

--- resources/views/produtos/trocaPreco.blade.php
+++ resources/views/produtos/trocaPreco.blade.php
@@ -50,13 +50,13 @@
                 <div class="row">
                     @foreach ($dadosResponse as $dado)
                         <div class="col-md-4 mb-4">
-                            <div class="card shadow text-primary text-center h-100"
+                            <div class="card shadow text-white text-center h-100"
                                 style="background-color: #464555; border-radius: 10px;">
                                 <div class="card-body d-flex flex-column">
                                     <h3 class="mb-3">{{ $dado->dscprod }}</h3>
                                     <p>A Vista - R$ {{ $dado->avista }}</p>
                                     <p>A Prazo - R$ {{ $dado->aprazo }}</p>
-                                    <button type="button" class="btn btn-outline-primary w-100 mt-auto"
+                                    <button type="button" class="btn btn-outline-light w-100 mt-auto"
                                         data-bs-toggle="modal" data-bs-target="#editPreco-{{ $loop->index }}">
                                         Trocar Preço
                                     </button>
