2848dff391e39d838a7210ecc32ac5c73932a52e
Matheus <matheuscontato96@hotmail.com>
2024-10-08 16:57:00 -0300

    feat: atualizando cards de formas de pagamento

--- resources/views/admin/caixa/resumoCaixa.blade.php
+++ resources/views/admin/caixa/resumoCaixa.blade.php
@@ -4,7 +4,7 @@
         @media (max-width: 768px) {
             .carousel-inner {
                 height: 35rem;
-                margin-left: 0.75rem;
+                margin-left: 1rem;
             }
         }
 
